5793274b82e5bbb10c3e948917b6b59e4f73e8d4
Walter <wcarpiom@ucsm.edu.pe>
2016-08-03 19:12:25 -0500

    Example of WCF application

---
+++ Client/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Client.HelloWorldServiceReference;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorldClient client = new HelloWorldClient("NetTcpBinding_IHelloWorld");
+            Name person = new Name();
+            person.First = "Gonzalo";
+            person.Last = "Garcia";
+            Console.WriteLine(client.SayHello(person));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HelloWorldService/HelloWorldService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace HelloWorldService
+{
+    [DataContract]
+    public class Name
+    {
+        [DataMember]
+        public string First;
+        [DataMember]
+        public string Last;
+    }
+
+    [ServiceContract]
+    public interface IHelloWorld
+    {
+        [OperationContract]
+        string SayHello(Name person);
+    }
+
+    public class HelloWorldService : IHelloWorld
+    {
+        public string SayHello(Name person)
+        {
+            return String.Format("Hello {0} {1}", person.First, person.Last);
+        }
+    }
+}
